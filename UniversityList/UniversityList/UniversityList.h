cefec45d7f871da76c92e725849513f9a0a23b4f
Mahmoud-abdelwahab-iOS <152803379+Mahmoud-abdelwahab-iOS@users.noreply.github.com>
2024-05-02 17:08:46 +0300

    adding modules

---
+++ UniversityList/UniversityList/UniversityList.h
@@ -0,0 +1,18 @@
+//
+//  UniversityList.h
+//  UniversityList
+//
+//  Created by Mahmoud Abdelwahab on 02/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UniversityList.
+FOUNDATION_EXPORT double UniversityListVersionNumber;
+
+//! Project version string for UniversityList.
+FOUNDATION_EXPORT const unsigned char UniversityListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniversityList/PublicHeader.h>
+
+
