79edf4a3a9ee7fdd64dd07b86f3e9ea58ed79b60
Mahmoud-abdelwahab-iOS <152803379+Mahmoud-abdelwahab-iOS@users.noreply.github.com>
2024-05-02 17:20:33 +0300

    init app

--- MainApp/MainApp/AppDelegate.swift
+++ MainApp/MainApp/AppDelegate.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import UniversityList
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+    var window: UIWindow?
+
+      func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         
+          window = UIWindow(frame: UIScreen.main.bounds)
+          let vc = UniversityListViewController()
+          window?.rootViewController = UINavigationController(rootViewController: vc)
+          window?.makeKeyAndVisible()
+          return true
+      }
 }
 
