bdcac9c9c88ae925edc0cde9fd9d18022f0ac2e4
Mahmoud-abdelwahab-iOS <152803379+Mahmoud-abdelwahab-iOS@users.noreply.github.com>
2024-05-03 01:05:48 +0300

    setup caching

--- MainApp/MainApp/RealmManager/RealmManager.swift
+++ MainApp/MainApp/RealmManager/RealmManager.swift
@@ -24,21 +24,25 @@ public class RealmManager {
 extension RealmManager: UniversityCachingProtocol {
     
     public func saveUniversitiesToRealmDB(_ universities: [UniversityCellVM]) throws {
-        do {
-            try realm.write {
-                realm.deleteAll()
-                for university in universities {
-                    let universityObject = UniversityObject()
-                    universityObject.name = university.name
-                    universityObject.stateProvince = university.stateProvince
-                    universityObject.webPage = university.webPage
-                    universityObject.alphaTwoCode = university.countryCode
-                    universityObject.country = university.country
-                    realm.add(universityObject)
+        DispatchQueue.main.async {
+            do {
+                let realm = try Realm()
+                try realm.write {
+                    realm.deleteAll()
+                    for university in universities {
+                        let universityObject = UniversityObject()
+                        universityObject.name = university.name
+                        universityObject.stateProvince = university.stateProvince
+                        universityObject.webPage = university.webPage
+                        universityObject.alphaTwoCode = university.countryCode
+                        universityObject.country = university.country
+                        realm.add(universityObject)
+                    }
                 }
+            } catch {
+                // Handle error
+                print("Error saving universities to Realm: \(error)")
             }
-        } catch {
-            throw error
         }
     }
     
